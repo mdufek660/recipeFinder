9f17526565f34a6e1bee63e7d57176e2f6559e77
Michael <mdufek.660@gmail.com>
2022-01-10 23:59:26 -0500

    First draft of project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Recipe Finder
+
+Final project for Javascript Fundamentals Fall 2021 with Albany Can Code. The goal of this project was to apply what was learned throughout the course, and demonstrate how to use an API, combined with user input, to generate dynamic content. To acheive these goals, I used [Edamam](https://www.edamam.com/) and their Recipe Search API to create a website capable of gathering recipes from across the internet.
+
+# Usage
+
+The Edamam API being utilized is the recipe search API. The API key for this project is being stored within the javascript for demo purposes, but should you be interested, you could get your own API key (and App ID) for free from https://developer.edamam.com and replace the constants at lines 4 and 5 to build off of my project.
+
+# Contact
+Author: Michael Dufek, https://github.com/mdufek660 or mdufek.660@gmail.com
+Project link: https://github.com/mdufek660/recipeFinder
--- js/main.js
+++ js/main.js
@@ -1,7 +1,154 @@
 (function () {
-	
-  let searchQuery = "";
-  const API_ID = '385c620c';
-  const API_key = "2ab3737a884c056d1768e98eee1d0087";
 
+  let searchQuery = "";
+  let displayThreshold=4;
+  const API_ID = '385c620c';
+  const API_KEY = "2ab3737a884c056d1768e98eee1d0087";
+
+  const FIRST_HALF_URL = "https://api.edamam.com/api/recipes/v2?type=public&q=";
+  const SECOND_HALF_URL= "&app_id="+API_ID+"&app_key="+API_KEY;
+
+  let resultsToHide;
+  let returnedRecipes;
+
+  document.getElementById("toggleDisplayAllResults").style.display='none';
+  document.getElementById("infoParent").style.display='none';
+
+  searchText.addEventListener('submit', (e) => {
+    e.preventDefault();
+    searchQuery = e.target.querySelector('input').value;
+    callAPI();
+  	document.getElementById("infoParent").style.display='none';
+  });
+
+  async function callAPI(){
+  	const apiURLPath=FIRST_HALF_URL+searchQuery+SECOND_HALF_URL;
+  	const response = await fetch(apiURLPath);
+  	const data = await response.json();
+  	returnedRecipes=data.hits;
+  	utilizeAPIResponse(data.hits);
+  }
+
+  function utilizeAPIResponse(results){
+  	console.log("hello");
+  	let generatedData = "";
+  	let counter = 0;
+  	let oddOrEven="";
+
+    results.map((result) => {
+    	if(counter%2==0){oddOrEven="evenItem"}
+    	else{oddOrEven="oddItem"}
+
+    	if(counter<displayThreshold){
+      generatedData += `
+	  <div class="item alwaysShow `+oddOrEven+`">
+	    <h2 class="recipe-title">${result.recipe.label}</h2>
+	  	<img src="${result.recipe.image}">
+	    <div class="flex-container anchor-left">
+	    <a class="buttonDesign anchor-left" href="${result.recipe.url}" target="_blank">View Recipe</a>
+	    <a class="buttonDesign anchor-right" id="`+counter+`">Nutrition & Ingredients</a>
+	  </div>
+	  </div>`;
+	}
+	else{
+      generatedData += `
+	  <div class="item toggleShow `+oddOrEven+`" >
+	    <h2 class="recipe-title">${result.recipe.label}</h2>
+	  <img src="${result.recipe.image}">
+	    <div class="flex-container">
+	    <a class="buttonDesign anchor-left" href="${result.recipe.url}" target="_blank">View Recipe</a>
+	    <a class="buttonDesign anchor-right" id="`+counter+`">Nutrition & Ingredients</a>
+	  </div>
+	  </div>`;
+	}
+	counter++;
+    });
+    searchResultsContainer.innerHTML = generatedData;
+    let resultsToHide=document.getElementsByClassName("toggleShow");
+    for(let entry of resultsToHide){
+    	entry.style.display='none';
+    }
+  	document.getElementById("toggleDisplayAllResults").style.display="block";
+
+	  try{
+	  	document.getElementById('0').onclick=updateNutritionBox;
+	  	document.getElementById('1').onclick=updateNutritionBox;
+	  	document.getElementById('2').onclick=updateNutritionBox;
+	  	document.getElementById('3').onclick=updateNutritionBox;
+	  	document.getElementById('4').onclick=updateNutritionBox;
+	  	document.getElementById('5').onclick=updateNutritionBox;
+	  	document.getElementById('6').onclick=updateNutritionBox;
+	  	document.getElementById('7').onclick=updateNutritionBox;
+	  	document.getElementById('8').onclick=updateNutritionBox;
+	  	document.getElementById('9').onclick=updateNutritionBox;
+	  	document.getElementById('10').onclick=updateNutritionBox;
+	  	document.getElementById('11').onclick=updateNutritionBox;
+	  	document.getElementById('12').onclick=updateNutritionBox;
+	  	document.getElementById('13').onclick=updateNutritionBox;
+	  	document.getElementById('14').onclick=updateNutritionBox;
+	  	document.getElementById('15').onclick=updateNutritionBox;
+	  	document.getElementById('16').onclick=updateNutritionBox;
+	  	document.getElementById('17').onclick=updateNutritionBox;
+	  	document.getElementById('18').onclick=updateNutritionBox;
+	  	document.getElementById('19').onclick=updateNutritionBox;  
+	  }
+	  catch(e){
+	  	console.log(e);
+	  }
+  }
+
+  let clicked=false;
+  document.getElementById("toggleDisplayAllResults").addEventListener("click", function(){
+  		if(!clicked){
+  			let resultsToShow=document.getElementsByClassName("toggleShow");
+		    for(let entry of resultsToShow){
+		    	entry.style.display='block';
+		    }
+		    clicked=true;
+		    document.getElementById("toggleDisplayAllResults").innerHTML="Less Results";
+  		}
+  		else{
+  			let resultsToHide=document.getElementsByClassName("toggleShow");
+		    for(let entry of resultsToHide){
+		    	entry.style.display='none';
+		    }
+		    clicked=false;
+		    document.getElementById("toggleDisplayAllResults").innerHTML="More Results";
+  		}
+  })
+/*
+	   <p class="calories">Calories: ${result.recipe.calories.toFixed(2)}</p>
+	   <p class="fat">Fat: ${result.recipe.digest[0].total.toFixed(2)}</p>
+	   <p class="protein">Protein: ${result.recipe.digest[2].total.toFixed(2)}</p>
+	   <p class="carbs">Carbohydrates: ${result.recipe.digest[1].total.toFixed(2)}</p>
+*/
+
+  const updateNutritionBox = function(){
+  	console.log("This is the button that triggered me: "+this.id);
+  	console.log(returnedRecipes[this.id].recipe.calories.toFixed(2))
+  	document.getElementById("infoParent").style.display="flex";
+  	console.log(returnedRecipes[this.id])
+  	let newNutritionalData=`
+  		<p id="calories">Calories: ${returnedRecipes[this.id].recipe.calories.toFixed(2)}</p>
+  	`
+  	for(let entry of returnedRecipes[this.id].recipe.digest){
+  		try{
+  			newNutritionalData+="<p id=\""+entry.label+"\">"+entry.label+": "+entry.total.toFixed(2)+"</p>"
+  		}
+  		catch(e){
+
+  		}
+  	}
+  	document.getElementById("nutritionInfoHere").innerHTML=newNutritionalData;
+
+  	let ingredientData="";
+  	let index=0;
+  	for(let entry of returnedRecipes[this.id].recipe.ingredients){
+  		try{
+  			ingredientData+="<p id=\"ingredient-"+index+"\">"+entry.text+"</p>"
+  		}
+  		catch(e){}
+  	}
+  	document.getElementById("ingredientInfoHere").innerHTML=ingredientData;
+  }
 })();
